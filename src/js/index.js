5c10268fc7da545ee80530ed3d1ac8c5ba5e0923
davisj08 <107017317+davisj08@users.noreply.github.com>
2024-01-29 21:39:49 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const botoes = document.querySelectorAll(".btn-plataforma");
+    console.log(botoes); // Verifique se os botões estão sendo selecionados corretamente
 
     botoes.forEach(function(botao) {
         botao.addEventListener("click", () => {
+            console.log("Botão clicado"); /
             botao.querySelector(".plataformas").classList.toggle("ativo");
         });
 
         const handleImageClick = (link) => {
-            window.open(link, "_blank");
+            console.log("Link clicado:", link); // Verifique se o link está sendo capturado corretamente
+            window.open(link, "_blank"); 
         };
 
         botao.querySelectorAll('img').forEach(function(img) {
             img.addEventListener('click', function() {
+                console.log("Imagem clicada"); // Verifique se a imagem está sendo clicada
                 var link = this.getAttribute('data-link');
                 handleImageClick(link);
             });
